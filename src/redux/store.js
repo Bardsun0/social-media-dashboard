5bd6c4d6e0f3cfea98ec4dd3e9b595301cc63a8d
Ozan Güneş <ozangunes2894@gmail.com>
2024-07-30 19:58:25 +0300

    Firebase - trying some stuff!

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { createSerializableStateInvariantMiddleware } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
-
-const serializableMiddleware = createSerializableStateInvariantMiddleware({
-  ignoredActions: ["auth/loginSuccess", "auth/registerSuccess"],
-  ignoredActionPaths: ["payload.user"],
-  ignoredPaths: ["auth.user"],
-});
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    // diğer reducer'lar...
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(serializableMiddleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["auth/loginSuccess", "auth/registerSuccess"],
+        ignoredActionPaths: ["payload.user"],
+        ignoredPaths: ["auth.user"],
+      },
+    }),
 });
